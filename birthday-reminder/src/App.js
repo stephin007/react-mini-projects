9c2ecf98f0a9c0a0d1f7dcf32dcc00b89ab9358e
stephin007 <stephinreji123@gmail.com>
2021-03-29 17:45:07 +0530

    Added the basic code needed

--- birthday-reminder/src/App.js
+++ birthday-reminder/src/App.js
@@ -1,8 +1,26 @@
+import {useState} from "react"
+import List from "./List"
+import data from "./data"
+
+
 function App() {
+  const [people,setPeople] = useState(data)
+
   return (
-    <div className="App">
-      hello
-    </div>
+    <main>
+      <section class="container">
+        <h3>
+          {people.length} Birthdays Today
+        </h3>
+        <List people={people}/>
+        <button onClick={() => setPeople([])}>
+          Clear all
+        </button>
+        <button onClick={() => setPeople(data)}>
+          Add all
+        </button>
+      </section>
+    </main>
   );
 }
 
