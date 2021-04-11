444f8c1348a687749b2081782ffbe5f5e784f8a5
stephin007 <stephinreji123@gmail.com>
2021-04-11 17:29:56 +0530

    Added prev and next functionality

--- reviews-card/src/Review.js
+++ reviews-card/src/Review.js
@@ -5,6 +5,26 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 const Review = () => {
     const [index, setIndex] = useState(0)
     const {name, job, image, text} = people[index]
+
+    // checkIndex function will check if the index goes more the length of the array or less than 0. If its more , it will make the index to 0 and if its less than 0 it will change the index to the last item of the array
+    
+
+
+    // prevPerson and nextPerson help in increasing or decreasing the index of the array.
+    const nextPerson = () => {
+      setIndex((index) => {
+        let newIndex = index + 1;
+        return (newIndex);
+      });
+    };
+
+    const prevPerson = () => {
+      setIndex((index) => {
+        let newIndex = index + 1;
+        return (newIndex);
+      });
+    };
+
     return (
       <article className="review">
         <div className="img-container">
@@ -17,16 +37,14 @@ const Review = () => {
         <p className="job">{job}</p>
         <p className="info">{text}</p>
         <div className="button-container">
-          <button className="prev-btn" >
+          <button className="prev-btn" onClick={prevPerson}>
             <FaChevronLeft />
           </button>
-          <button className="next-btn" >
+          <button className="next-btn" onClick={nextPerson}>
             <FaChevronRight />
           </button>
         </div>
-        <button className="random-btn" >
-          surprise me
-        </button>
+        <button className="random-btn">surprise me</button>
       </article>
     );
 }
