dc86c8a6f01c5b0363dc650365a8d9c782b498c1
stephin007 <stephinreji123@gmail.com>
2021-04-23 23:37:23 +0530

    Added category filters

--- category-menu/src/App.js
+++ category-menu/src/App.js
@@ -1,16 +1,28 @@
 import {useState} from "react"
 import Menu from "./Menu"
+import Categories from "./Categories";
 import items from "./data"
 
+const allCategories = ["all", ...new Set(items.map((item) => item.category))]
 
 function App() {
     const [menuItems, setMenuItems] = useState(items)
+    const[categories, setcategories] = useState(allCategories)
+    const filterItems = (category) => {
+        if(category === "all"){
+            setMenuItems(items)
+            return
+        }
+        const newItems = items.filter((item) => item.category === category)
+        setMenuItems(newItems)
+    }
   return (
     <section className="menu section">
       <div className="title">
           <h2>Our Menu</h2>
           <div className="underline"/>
       </div>
+        <Categories categories={categories} filterItems={filterItems}/>
         <Menu menuItems={menuItems}/>
     </section>
   );
---
+++ category-menu/src/Categories.js
@@ -0,0 +1,17 @@
+const Categories = ({categories, filterItems}) => {
+    return(
+        <>
+            <div className="btn-container">
+                {
+                    categories.map((category, index)=> {
+                        return <button type="button" key={index} className="filter-btn" onClick={()=> filterItems(category)}>
+                            {category}
+                        </button>
+                    })
+                }
+            </div>
+        </>
+    )
+}
+
+export default Categories
