f71cf09843fec273a20f5b65fb36dc5ac5efb9b9
stephin007 <stephinreji123@gmail.com>
2021-07-23 13:40:47 +0530

    Added sidebar links

--- sidebar-modal-contextapi/src/components/Sidebar.js
+++ sidebar-modal-contextapi/src/components/Sidebar.js
@@ -1,7 +1,41 @@
-import React from "react";
+import logo from "../logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { social, links } from "../data";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-header">
+        <img src={logo} alt="logo" className="logo" />
+        <button className="close-btn">
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, icon, text } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="social-icons">
+        {social.map((link) => {
+          const { id, url, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
 };
 
 export default Sidebar;
